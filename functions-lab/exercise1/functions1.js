6757f0c22e1ba6fae76394a1620b1f4e329d53f9
msrur <msrur@ebay.com>
2020-10-11 20:01:35 +0300

    making master empty

--- functions-lab/exercise1/functions1.js
+++
@@ -1,8 +0,0 @@
-var btn = document.querySelector('button');
-var text = document.querySelector('p');
-
-function swapText() {
-  text.innerHTML = 'Nice To Meet You';
-}
-
-btn.addEventListener('click', swapText);
